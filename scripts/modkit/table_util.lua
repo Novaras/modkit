6c6f4aa8b623104b09f53fefe7619903e5f3d6dc
Novaras <tfear091@gmail.com>
2021-06-05 11:10:38 +0100

    find fn + fix printtbl

--- scripts/modkit/table_util.lua
+++ scripts/modkit/table_util.lua
@@ -1,38 +1,36 @@
 --Table utility functions.
 
+function _printTbl(table, indent)
+	if (indent == nil) then
+		indent = 0;
+	end
+	local indent_str = "";
+	if (indent > 0) then
+		local cur_indents = 0;
+		while (cur_indents ~= indent) do
+			indent_str = indent_str .. "\t";
+			cur_indents = cur_indents + 1;
+		end
+	end
+	for k, v in table do
+		if type(v) == "table" then
+			print(indent_str .. "\"" .. k .. "\": {");
+			_printTbl(v, indent + 1);
+			print(indent_str .. "},");
+		else
+			if (type(v) ~= "number") then
+				v = "\"" .. tostring(v) .. "\"";
+			end
+			print(indent_str .. "\"" .. k .. "\": " .. v .. ',');
+		end
+	end
+end
+
 if (modkit == nil) then modkit = {}; end
 
 if (modkit.table == nil) then
-	modkit.table = {}
-	modkit.table._printTbl = function (tbl, indent)
-		if (indent == nil) then
-			indent = 0;
-		end
-		local indent_str = "";
-		if (indent > 0) then
-			local cur_indents = 0;
-			while (cur_indents ~= indent) do
-				indent_str = indent_str .. "\t";
-				cur_indents = cur_indents + 1;
-			end
-		end
-		for k, v in tbl do
-			if type(v) == "table" then
-				print(indent_str .. "\"" .. k .. "\": {");
-				_printTbl(v, indent + 1, self);
-				print(indent_str .. "},");
-			else
-				if (type(v) ~= "number") then
-					v = "\"" .. tostring(v) .. "\"";
-				end
-				print(indent_str .. "\"" .. k .. "\": " .. v .. ',');
-			end
-		end
-	end
 
-
-
-	modkit.table = {
+	local table = {
 		filter = function (table, predicate)
 			local out = {};
 			for i, v in table do
@@ -90,14 +88,28 @@ if (modkit.table == nil) then
 			end
 			return false;
 		end,
-		printTbl = function (tbl, label)
-			if (label == nil) then
-				label = tostring(tbl);
+		find = function (table, predicate)
+			for i, v in table do
+				if (predicate(v, i, table) ~= nil) then
+					return v;
+				end
 			end
-			local temp_tbl = {};
-			temp_tbl[label] = tbl;
-			_printTbl(temp_tbl);
 		end
-	}
+	};
+
+	function table.printTbl(table, label)
+		if (label == nil) then
+			label = tostring(table);
+		end
+		local temp_table = {};
+		temp_table[label] = table;
+		_printTbl(table);
+	end
+
+	modkit.table = {};
+	for k, v in table do
+		modkit.table[k] = v;
+	end
+
 	print("table_util init");
 end
