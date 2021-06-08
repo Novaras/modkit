c52c43be4033f6ad72eb879e792b00a9354d38b9
Novaras <tfear091@gmail.com>
2021-06-09 00:23:32 +0100

    new composition method; client syntax unchanged, but can now have 'base' prototypes

--- scripts/driver.lua
+++ scripts/driver.lua
@@ -10,52 +10,12 @@ if (H_DRIVER == nil) then
 		dofilepath("data:scripts/modkit.lua");
 	end
 
-	if (CUSTOM_CODE == nil) then
-		dofilepath("data:scripts/custom_code/custom_code.lua");
-	end
-
-	if (GLOBAL_PROTOTYPE == nil or GLOBAL_PROTO_KEY == nil) then
-		dofilepath("data:scripts/custom_code/global_attribs.lua");
-	end
-
-	-- Reads the custom attributes defined for the type of ship indexed by `type_group`.
-	-- If you had CUSTOM_CODE["kus_lightcorvette"] defined under custom_code/kus/, here we would load the custom attributes
-	-- and methods you defined.
-	function GetCustomAttribs(type_group, player, id, type_alias)
-		local as_lower = strlower(type_group);
-		if (CUSTOM_CODE[as_lower]) then
-			local definition = CUSTOM_CODE[as_lower];
-			
-			local attribs;
-			if (type(definition.attribs) == "table") then
-				attribs = definition.attribs;
-			else
-				attribs = definition.attribs(type_alias or type_group, player, id);
-			end
-
-			-- ensure typed
-			if (attribs.type_group == nil) then
-				attribs.type_group = type_group;
-			end
-
-			if (attribs.own_group == nil) then
-				attribs.own_group = SobGroup_Fresh(type_group .. "-" .. id)
-			end
-
-			local result = modkit.table.merge(
-				definition,
-				attribs
-			);
-			result.attribs = nil; -- remove constructor from output
-			return result;
-		end
-		return {};
-	end
 	GLOBAL_REGISTER = modkit.MemGroup.Create("mg-global");
 
 	--- Registers the incoming sobgroup, player index, and ship id into a Ship table within the global registry.
 	-- The Ship is a rich representation of the actual ingame ship as a proper workable table.
 	function register(type_group, player_index, ship_id)
+		type_group = strlower(type_group); -- immediately make this lowercase
 		local caller = GLOBAL_REGISTER:get(ship_id);
 		if (caller ~= nil) then -- fast return if already exists
 			return caller;
@@ -65,10 +25,7 @@ if (H_DRIVER == nil) then
 		-- somewhere in `custom_code/<race>/<custom-ship>.lua`.
 		local caller = GLOBAL_REGISTER:set(
 			ship_id,
-			modkit.table.merge(
-				GetCustomAttribs(GLOBAL_PROTO_KEY, player, ship_id, type_group),	-- stuff set in global_attribs
-				GetCustomAttribs(type_group, player, ship_id)						-- attribs and create/update/destroy custom fns,
-			)
+			modkit.compose:instantiate(type_group, player, ship_id)
 		);
 		-- ensure non-nil when calling these:
 		for i, v in {
@@ -84,17 +41,14 @@ if (H_DRIVER == nil) then
 				caller[v] = NOOP;
 			end
 		end
+
 		return caller;
 	end
 
 	-- === load, create, update, destroy ===
 
-	function load(g, p, i)
-		local caller = register(g, p, i);
-
-		caller:load();
-
-		return caller;
+	function load(load)
+		return NOOP;
 	end
 	
 	function create(g, p, i)
@@ -116,17 +70,17 @@ if (H_DRIVER == nil) then
 
 		caller:update(); -- run the caller's custom update hook
 
-		local past_self = {};
-		for k, v in caller do
-			if (k ~= 'past_self') then -- very important to prevent memory pileup!
-				if type(v) == 'function' then
-					past_self[k] = caller[k](caller); -- collapse getters into values
-				else
-					past_self[k] = v;
-				end
-			end
-		end
-		caller.past_self = past_self; -- for next run comparisons
+		-- local past_self = {};
+		-- for k, v in caller do
+		-- 	if (k ~= 'past_self') then -- very important to prevent memory pileup!
+		-- 		if type(v) == 'function' then
+		-- 			past_self[k] = caller[k](caller); -- collapse getters into values
+		-- 		else
+		-- 			past_self[k] = v;
+		-- 		end
+		-- 	end
+		-- end
+		-- caller.past_self = past_self; -- for next run comparisons
 
 		return caller;
 	end
---
+++ scripts/modkit/compose.lua
@@ -0,0 +1,62 @@
+if (modkit == nil) then modkit = {}; end
+
+if (modkit.compose == nil) then
+	local compose = {
+		_base = {},
+		_ship = {}
+	};
+
+	function compose:addBaseProto(proto)
+		self._base[getn(self._base) + 1] = proto;
+	end
+
+	function compose:addShipProto(type, proto)
+		self._ship[type] = proto;
+	end
+
+	function compose:instantiate(type_group, player, id)
+		local out_ship = {};
+
+		local source = modkit.table:merge(
+			self._base,
+			{
+				[getn(self._base) + 1] = self._ship[type_group]
+			}
+		);
+
+		for _, proto in source do
+			for k, prop in proto do
+				if (k == "attribs") then
+					local result = {};
+					if (type(prop) == "function") then -- resolve attribs if fn
+						result = prop(type_group, player, id);
+					else
+						result = prop;
+					end
+					out_ship = modkit.table:merge(
+						out_ship or {},
+						result
+					);
+				else
+					out_ship[k] = prop;
+				end
+			end
+		end
+
+		print("out:");
+		modkit.table.printTbl(out_ship);
+		print("/out");
+
+		return out_ship;
+	end
+
+
+	modkit.compose = compose;
+
+	doscanpath("data:scripts/custom_code/lib", "*.lua");
+
+	doscanpath("data:scripts/custom_code/hgn", "*.lua");
+	doscanpath("data:scripts/custom_code/vgr", "*.lua");
+	doscanpath("data:scripts/custom_code/kus", "*.lua");
+	doscanpath("data:scripts/custom_code/tai", "*.lua");
+end
