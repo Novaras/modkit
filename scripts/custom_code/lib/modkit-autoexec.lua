478906b1c3e3adc59bae1b5b580f1c2ef2138f3b
Novaras <tfear091@gmail.com>
2021-06-18 01:13:22 +0100

    predicate-based wait instead of hardcode timeout

--- scripts/custom_code/lib/modkit-autoexec.lua
+++ scripts/custom_code/lib/modkit-autoexec.lua
@@ -2,18 +2,21 @@ modkit_autoexec = {
 	_move_action = { -- state used by move-action functions (moveActionQueue), also see below for methods attached on _ma
 		index = 1,
 		move_actions = {},
-		acceptable_distance = 100,
+		distance_threshold = 100,
 		arrived_tick = -1,
-		wait_duration = 4,
-		waiting = 0
+		actioning = 0,
 	}
 };
 
 -- lua though
 local _ma = modkit_autoexec._move_action;
 
+function _ma.defaultPredicate(caller)
+	return caller:tick() >= modkit_autoexec._move_action.arrived_tick + 4;
+end
+
 --- Returns the next moveaction (the current destination).
--- This will always be a table with 'position' and 'actionFn' properties - if you
+-- This will always be a table with 'position' and 'action' properties - if you
 -- passed in a raw position table it will be packed into the 'position' property.
 function _ma:next()
 	local next = self.move_actions[self.index];
@@ -21,52 +24,53 @@ function _ma:next()
 		return next;
 	end
 	if (next.position == nil) then -- 'next' is just a position, create a moveaction out of it
-		return {
-			position = next,
-			actionFn = function () end
+		next = {
+			position = next
 		};
 	end
+	next.action = next.action or function () end
+	-- default finish predicate just checks to see if the ship arrived at least 4 ticks ago
+	next.finished = next.finished or self.defaultPredicate;
 	return next;
 end
 
---- Returns whether or not this current wait period should end.
-function _ma:finished(tick)
-	return self.waiting == 1 and self.arrived_tick + (self:next().wait_duration or self.wait_duration) <= tick;
+--- Returns whether or not this current move-action is over
+function _ma:finished(caller)
+	return self.actioning == 1 and self:next().finished(caller, self);
 end
 
 --- Takes a list of 'move actions', and executes them one by one until completion (using driver's auto-exec feature).
 -- The next move-action is not started until the current one has been completed. On arrival to a position
--- (which is an xyz position table or a valid volume name), the ship will enter a 'waiting' phase where it has time
+-- (which is an xyz position table or a valid volume name), the ship will enter a 'actioning' phase where it has time
 -- to perform any actions contained in the supplied callback, if given.
 -- @param move_actions [table] The table of move-actions. See above for the definition of a 'move-action'.
 -- @param wait_duration [number] The global wait duration for any move-action without its own custom wait duration.
--- @param acceptable_distance [number] The distance the ship must get within relative to its destination before it enters its waiting phase
+-- @param distance_threshold [number] The distance the ship must get within relative to its destination before it enters its actioning phase
 -- @return nil
-function modkit_autoexec:moveActionQueue(move_actions, wait_duration, acceptable_distance)
+function modkit_autoexec:moveActionQueue(move_actions, distance_threshold, defaultPredicate)
 	local MA = self._move_action;
 
-	if (move_actions) then
+	if (move_actions) then -- new set
 		MA.move_actions = move_actions;
-		MA.waiting = 0;
+		MA.actioning = 0;
 		MA.index = 1;
-		MA.arrived_tick = -1;
 	end
 
 	-- defaults
-	MA.wait_duration = wait_duration or MA.wait_duration;
-	MA.acceptable_distance = acceptable_distance or MA.acceptable_distance;
+	MA.distance_threshold = distance_threshold or MA.distance_threshold;
+	MA.defaultPredicate = defaultPredicate or MA.defaultPredicate;
 
 	-- attach this fn to the auto exec list (so the base update fn doesn't need to call anything)
 	local runner = function (caller)
 		local next = %MA:next();
 		if (next) then
-			if (%MA.waiting == 0 and %self:distanceTo(next.position) <= %MA.acceptable_distance) then -- if not waiting and out of range
+			if (%MA.actioning == 0 and %self:distanceTo(next.position) <= %MA.distance_threshold) then -- if not actioning and out of range
+				%MA.actioning = 1;
 				%MA.arrived_tick = %self:tick();
-				%MA.waiting = 1;
-				next.actionFn(%self);
-			elseif (%MA:finished(%self:tick()) and modkit.table.length(%MA.move_actions) > %MA.index) then -- finished wait and not final move-action
+				next.action(%self);
+			elseif (%MA:finished(%self) and modkit.table.length(%MA.move_actions) > %MA.index) then -- finished action and not final move-action
 				%MA.index = %MA.index + 1;
-				%MA.waiting = 0;
+				%MA.actioning = 0;
 				%self:move(%MA:next().position);
 			elseif (%MA.index == 1) then -- first move-action, just go
 				%self:move(%MA:next().position);
@@ -74,7 +78,7 @@ function modkit_autoexec:moveActionQueue(move_actions, wait_duration, acceptable
 		end
 	end
 
-	modkit.table.push(self.auto_exec, runner); -- hook to autoexec
+	self.auto_exec["move-actions" .. self.id] = runner; -- hook to autoexec
 
 end
 
