212b957b40cba6bb5eef9c6accc427dc94966873
Novaras <tfear091@gmail.com>
2022-11-28 19:22:32 +0000

    driver publish ids to ui scope

--- scripts/modkit.lua
+++ scripts/modkit.lua
@@ -2,12 +2,13 @@
 -- This is the file you want to load in your scripts with dofilepath, as it will load
 -- the package in order, providing the modkit table and a collection of custom sobgroup functions
 
+loadModkit = loadModkit or function ()
+	modkit = {};
+	doscanpath("data:scripts/modkit", "*.lua");
+end
+
 if (modkit == nil) then -- header guard
 	print("\n\nmodkit.lua init...");
-
-	modkit = {};
-	
-	doscanpath("data:scripts/modkit", "*.lua");
-
+	loadModkit();
 	print("modkit.lua loaded successfully!\n\n");
 end
