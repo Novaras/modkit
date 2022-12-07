cf2851c0ecad026bdb7c155473c4107a92bfc696
Novaras <tfear091@gmail.com>
2022-12-07 18:17:34 +0000

    less bug-prone init

--- scripts/modkit/console.lua
+++ scripts/modkit/console.lua
@@ -1,6 +1,8 @@
 if (MODKIT_CONSOLE == nil) then
+	dofilepath("data:scripts/modkit/scope_state.lua");
 	doscanpath("data:scripts/modkit/console", "*.lua");
-	consoleLog("Console running...");
+
+	print("Console init...");
 
 	MODKIT_CONSOLE = 1;
 end
