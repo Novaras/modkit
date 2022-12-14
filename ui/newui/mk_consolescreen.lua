a15aba16593ee9cfbc7d81cad82c9b15ee19bb0a
Novaras <tfear091@gmail.com>
2022-12-14 14:40:51 +0000

    fix line height issues

--- ui/newui/mk_consolescreen.lua
+++ ui/newui/mk_consolescreen.lua
@@ -12,7 +12,7 @@ local makeLineBox = function (index, pt, color, name)
 		wrapping = 0,
 		Layout = {
 			margin_RB = { r = 0, b = 0, rr = "px", br = "px" },
-			size_WH = { w = 1, h = 18, wr = "par", hr = "px" },
+			size_WH = { w = 1, h = 20, wr = "par", hr = "px" },
 			pad_LT = { l = 0, t = pt, lr = "px", pt = "px" },
 		},
 		Text = {
@@ -52,7 +52,7 @@ MK_ConsoleScreen = {
 		TitleText =	"Modkit Console",
 		name = "mk_consolescreen_root",
 		Layout = {
-			size_WH = {w = 0.6, h = 550, wr = "scr", hr = "px",},
+			size_WH = {w = 0.6, h = 600, wr = "scr", hr = "px",},
 			pos_XY = {	x = 0.2, y = 0.2, xr = "scr", yr = "scr", },
 		},
 		arrangetype = "vert",
