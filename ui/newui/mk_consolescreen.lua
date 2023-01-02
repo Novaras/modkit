015a5ef97ac3218b5141fd4ce8c5eb48aabe30b3
Novaras <tfear091@gmail.com>
2023-01-02 17:11:31 +0000

    make slightly taller again;

--- ui/newui/mk_consolescreen.lua
+++ ui/newui/mk_consolescreen.lua
@@ -1,9 +1,11 @@
 dofilepath("data:ui/newui/Styles/HWRM_Style/HWRMDefines.lua");
 dofilepath("data:ui/newui/Styles/HWRM_Style/ControlConstructors.lua");
 
+dofilepath("data:scripts/modkit/table_util.lua");
+
 UI_LoadUILibrary("data:ui/newui/mk_consolescreencode.lua");
 
-local makeLineBox = function (index, pt, color, name)
+local makeLineBox = function (index, pt, color, name, pos, offset)
 	pt = pt or 0;
 	name = name or ("line" .. index);
 	return {
@@ -12,17 +14,19 @@ local makeLineBox = function (index, pt, color, name)
 		wrapping = 0,
 		Layout = {
 			margin_RB = { r = 0, b = 0, rr = "px", br = "px" },
-			size_WH = { w = 1, h = 20, wr = "par", hr = "px" },
+			size_WH = { w = 1, h = 22, wr = "par", hr = "px" },
 			pad_LT = { l = 0, t = pt, lr = "px", pt = "px" },
+			pos = pos,
 		},
 		Text = {
 			text = "",
 			textStyle = "FEHelpTipTextStyle",
 			vAlign = "Top",
 			color = color or { 210, 210, 210, 255 },
+			offset = offset or { 0, 0 }
 		},
 		giveParentMouseInput = 1,
-		--backgroundColor = {0,255,255,255},
+		-- backgroundColor = {0,255,255,255},
 	};
 end
 
@@ -52,8 +56,8 @@ MK_ConsoleScreen = {
 		TitleText =	"Modkit Console",
 		name = "mk_consolescreen_root",
 		Layout = {
-			size_WH = {w = 0.6, h = 600, wr = "scr", hr = "px",},
-			pos_XY = {	x = 0.2, y = 0.2, xr = "scr", yr = "scr", },
+			size_WH = { w = 0.6, h = 650, wr = "scr", hr = "px", },
+			pos_XY = {	x = 0.2, y = 0.15, xr = "scr", yr = "scr", },
 		},
 		arrangetype = "vert",
 		customDataString = "",
@@ -99,10 +103,60 @@ MK_ConsoleScreen = {
 			{ -- seperator
 				type = "Frame",
 				Layout = {
-					size_WH = {w = 1, h = 10, wr = "par", hr = "px",},
+					size_WH = {w = 1, h = 18, wr = "par", hr = "px",},
 				},
+				-- backgroundColor = {0,255,255,255},
 			},
-			makeLineBox(25, 0, { 50, 255, 255, 255 }, 'input_target'),
+			makeLineBox(25, 0, { 50, 255, 255, 255 }, 'input_target', {	x = 0, y = 1, xr = "par", yr = "par", }),
+			{ -- need a horizontal frame for the growing cursor offset / cursor
+				type = "Frame",
+				Layout = {
+					size_WH = { w = 1, h = 21, wr = 'par', hr = 'px', },
+				},
+				arrangetype = 'horiz',
+				;
+				{
+					type = "TextLabel",
+					name = 'cursor_offset',
+					wrapping = 0,
+					autosize = 1,
+					Layout = {
+						margin_RB = { r = 0, b = 0, rr = "px", br = "px" },
+						size_WH = { w = 100, h = 20, wr = "px", hr = "px" },
+						pad_LT = { l = 0, t = 0, lr = "px", pt = "px" },
+						pos = {	x = 0, y = 1, xr = "par", yr = "par", },
+					},
+					Text = {
+						text = "",
+						textStyle = "FEHelpTipTextStyle",
+						vAlign = "Top",
+						color = { 0, 255, 0, 0 },
+						offset = { -6, -15 }
+					},
+					giveParentMouseInput = 1,
+					--backgroundColor = {0,255,255,255},
+				},
+				{
+					type = "TextLabel",
+					name = 'cursor',
+					wrapping = 0,
+					Layout = {
+						margin_RB = { r = 0, b = 0, rr = "px", br = "px" },
+						size_WH = { w = 24, h = 20, wr = "px", hr = "px" },
+						pad_LT = { l = 0, t = 0, lr = "px", pt = "px" },
+						pos = {	x = 0, y = 1, xr = "par", yr = "par", },
+					},
+					Text = {
+						text = "<b>_</b><c=000000>0</c>",
+						textStyle = "FEHelpTipTextStyle",
+						vAlign = "Top",
+						color = { 255, 0, 0, 255 },
+						offset = { -6, -15 }
+					},
+					giveParentMouseInput = 1,
+					--backgroundColor = {0,255,255,255},
+				}
+			}
 		},
 	},
 };
