594c95cc7586037b3fe921388ffe575d5b79eddb
Novaras <tfear091@gmail.com>
2023-01-21 17:49:02 +0000

    check rule first

--- leveldata/multiplayer/lib/modkit-scheduler.lua
+++ leveldata/multiplayer/lib/modkit-scheduler.lua
@@ -7,5 +7,7 @@ function modkit_scheduler_spawn()
 	local volume = Volume_Fresh("modkit__scheduler_controller_vol");
 	SobGroup_SpawnNewShipInSobGroup(-1, "modkit_scheduler", group, group, volume);
 	print("modkit_scheduler spawn finished, check: (c: " .. SobGroup_Count(group) .. ", g: '" .. group .. "', pid: " .. -1 .. ")");
-	Rule_Remove("modkit_scheduler_spawn");
+	if (Rule_Exists("modkit_scheduler_spawn") == 1) then
+		Rule_Remove("modkit_scheduler_spawn");
+	end
 end
