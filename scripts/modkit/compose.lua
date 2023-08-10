8af8c59d87d9d7f1f3ba5484e91cdd995e1da137
Novaras <tfear091@gmail.com>
2023-08-10 17:43:47 +0100

    remove mod-specific

--- scripts/modkit/compose.lua
+++ scripts/modkit/compose.lua
@@ -153,5 +153,4 @@ if (modkit.compose == nil) then
 	doscanpath("data:scripts/custom_code/lib", "*.lua");
 
 	doscanpath("data:scripts/custom_code/hw1", "*.lua");
-	doscanpath("data:scripts/custom_code/horde/ship_scripts", "*.lua");
 end
