dff63ecc1ca9fb2f0b6dc78f203ddf80a0eb4240
Novaras <tfear091@gmail.com>
2023-08-10 17:44:11 +0100

    big update; campaign rules can chain with :next and :catch

--- scripts/modkit/campaign.lua
+++ scripts/modkit/campaign.lua
@@ -4,9 +4,33 @@
 if (modkit == nil) then modkit = {}; end
 if (modkit.MemGroup == nil) then dofilepath("data:scripts/modkit/memgroup.lua"); end
 
-if (H_CAMPAIGN == nil) then
+if (H_CAMPAIGN == nil or (modkit ~= nil and modkit.campaign == nil)) then
 	local campaign = {};
 	modkit.campaign = {};
+
+	--- Cast a `RuleFn` to a `Rule` (filling with defaults). Does nothing if `rule` is not a `function` type.
+	---
+	---@param rule Rule|RuleFn
+	---@return Rule
+	function toRule(rule)
+		if (type(rule) == "function") then
+			return modkit.campaign.rules:make(rule);
+		end
+
+		return rule;
+	end
+
+	---comment
+	---@param rule Rule|RuleFn
+	---@return RuleFn
+	function toRuleFn(rule)
+		if (type(rule) == "table" and rule.api_name) then
+			return rule.fn;
+		end
+		---@cast rule RuleFn
+
+		return rule;
+	end
 
 	if (GLOBAL_RULES == nil) then
 
@@ -15,35 +39,65 @@ if (H_CAMPAIGN == nil) then
 		---@field _interval integer
 		---@field _started_gametime number
 		---@field _tick integer
+		---@field _value any
 
-		---@class RuleState : RuleCoreState, table
+		---@class RuleState : RuleCoreState, any[]
 
-		---@alias RuleFn fun(state: RuleState, rules: Rules)
+		--- Call to end the rule executing this `RuleFn`. Args are included as the initial `_value` of the next chained rule, if any.
+		---@alias RuleResolve fun(...: any): nil
+
+		---@alias RuleError fun(message: string): nil
+
+		---@alias RuleFn fun(resolveCallback: RuleResolve, rejectCallback: RuleError, state: RuleState, rules: Rules): any
+
+		---@alias RuleStatus "init"|"running"|"resolved"|"rejected"
 
 		---@class Rule
+		---@overload fun(): RuleChain
 		---@field fn RuleFn
 		---@field id string
 		---@field interval integer
-		---@field status "init"|"running"|"returned"
+		---@field status RuleStatus
 		---@field api_name string
-		---@field value any|nil
 		---@field fn_state RuleState
 		---@field core_state RuleCoreState
+		---@field result any[] Table of values from the previous `RuleResolve` call in the chain
+		---@field error? string Error message passed to a `RuleError` call in the chain
+		---@field begin function
+		---@field run function
+		---@field finish function
 
-		---@class GLOBAL_RULES : MemGroup
+		---@class RuleChain
+		---@field next fun(self: RuleChain, action: Rule|RuleFn): RuleChain
+		---@field catch fun(self: RuleChain, action: function): nil
+
+		---@class Listener
+		---@field pattern string
+		---@field exec function
+		---@field callback RuleFn
+
+		---@class GLOBAL_RULES : MemGroupInst
+		---@field find fun(self: GLOBAL_RULES, predicate: fun(rule: Rule): boolean): Rule|nil
 		---@field _entities Rule[]
 		---@field __runner function|nil
-		---@field __listeners table
+		---@field __listeners Listener[]
 		---@field __level_path string
+		---@field __tick integer
 		GLOBAL_RULES = modkit.MemGroup.Create("mg-rules-global", {
 			__runner = nil,
 			__rule_pattern = nil,
 			__listeners = {},
-			__level_path = nil
+			__level_path = nil,
+			__tick = 0,
 		});
 
+		---
+		---@param id string
+		---@param rule_fn Rule|RuleFn
+		---@param interval number
+		---@param state RuleState
+		---@return Rule
 		function GLOBAL_RULES:add(id, rule_fn, interval, state)
-			---@type Rule
 			local rule = {
 				id = id,
 				interval = interval,
@@ -52,8 +106,8 @@ if (H_CAMPAIGN == nil) then
 				fn = rule_fn,
 				core_state = {
 					_rule_name = id,
-					_interval = interval
-				}
+					_interval = interval,
+				},
 			};
 
 			rule.fn_state = modkit.table:merge(
@@ -65,10 +119,20 @@ if (H_CAMPAIGN == nil) then
 				state
 			);
 
-			function rule:finish()
+			---
+			---@param status RuleStatus
+			---@param args any[]
+			function rule:finish(status, args)
 				print("kill rule " .. self.api_name);
+
 				Rule_Remove(self.api_name);
-				self.status = "returned";
+
+				self.status = status;
+
+				args.n = nil;
+				self.result = args;
+
+				-- modkit.table.printTbl(GLOBAL_RULES, "GLOBAL_RULES");
 			end
 
 			function rule:run()
@@ -78,28 +142,91 @@ if (H_CAMPAIGN == nil) then
 					self.core_state
 				);
 				self.fn_state._tick = self.fn_state._tick + 1;
-				local result = self.fn(self.fn_state, modkit.campaign.rules);
-				if (result) then
-					self:finish();
+
+				local resolve = function (...)
+					%self:finish("resolved", arg);
 				end
-				self.value = result; -- store in state!
-				return result;
+
+				local reject = function (message)
+					%self.error = message;
+					%self:finish("rejected", {});
+				end
+
+				self.core_state._value = self.fn(resolve, reject, self.fn_state, modkit.campaign.rules);
+				return self.core_state._value;
 			end
 
-			function rule:begin()
+			--- Begins this rule. This sets the status to `"running"` and calls `Rule_AddInterval` with the rule's `fn`.
+			---
+			---@param initial_value any
+			---@return RuleChain|nil
+			function rule:begin(initial_value)
 				if (self.status ~= "init") then
-					self:finish();
+					return nil;
 				end
 				self.fn_state._started_gametime = Universe_GameTime();
 				print("begin rule " .. self.api_name);
+				-- we create a global function which runs the rule's 'run' callback
 				GLOBAL_RULES.__runner = function ()
 					%self:run();
 				end;
+				-- here we bind this function to a key on `globals()`
 				dostring(self.api_name .. " = GLOBAL_RULES.__runner"); -- my word...
 				print(globals()[self.api_name]);
+				-- set the initial value (important for chaining)
+				self.core_state._value = initial_value;
+				-- and we then invoke it every `self.interval` ticks
 				Rule_AddInterval(self.api_name, self.interval);
 				self.status = "running";
+
+				---@param rule Rule|RuleFn
+				---@return RuleChain
+				_makeChain = function (rule)
+
+					rule = toRule(rule);
+
+					--- @class RuleChain
+					local chainable = {};
+
+					function chainable:next(action)
+						action = toRule(action);
+						local rule = %rule;
+						modkit.campaign.rules:on(rule.api_name, function ()
+							print("next listener for " .. %rule.api_name);
+							modkit.table.printTbl(%rule);
+							if (%rule.status == "rejected") then -- on a previous reject, we need to propagate it
+								%action.error = %rule.error;
+								%action:finish("rejected", {});
+							else
+								%action:begin(%rule.result);
+							end
+						end); -- when current rule is done, call `action:begin()`
+
+						return _makeChain(action); -- another action can be appended
+					end
+
+					function chainable:catch(handler)
+						local rule = %rule;
+						modkit.campaign.rules:on(rule.api_name, function ()
+							print("catch listener for " .. %rule.api_name);
+							%handler(%rule.error, %rule);
+						end);
+					end
+
+					return chainable;
+				end
+
+				return _makeChain(self);
 			end
+
+			-- here we add a tag on the rule which lets you call the rule like a function: `myrule.begin();` equivalent to `myrule();`
+			local rule_callable_tag = newtag();
+			settagmethod(rule_callable_tag, "function", function ()
+				return %rule:begin();
+			end);
+			settag(rule, rule_callable_tag);
+
+			---@cast rule Rule
 
 			return self:set(id, rule);
 		end
@@ -113,32 +240,36 @@ if (H_CAMPAIGN == nil) then
 		min_poll_interval = 0.1,
 	};
 
-	---@param name string
-	---@return Rule
+	---@param name string The given name or API name
+	---@return Rule|nil
 	function rules:get(name)
-		return GLOBAL_RULES:get(name);
+		return GLOBAL_RULES:get(name) or GLOBAL_RULES:find(function (rule)
+			return rule.id == %name or rule.api_name == %name;
+		end);
 	end
 
-	---@param name string
 	---@param rule_fn RuleFn
-	---@param interval integer
+	---@param interval? integer
+	---@param state? any[]
+	---@param name? string
 	---@return Rule
-	function rules:make(name, rule_fn, interval, state)
+	function rules:make(rule_fn, interval, state, name)
+		name = name or ("__rule__" .. tostring(GLOBAL_RULES.__tick) .. "_" .. tostring(modkit.table.length(GLOBAL_RULES._entities)));
+
 		interval = interval or self.min_poll_interval;
-		local already_exists = GLOBAL_RULES:find(function (rule)
-			return rule.name == %name;
-		end);
+		local already_exists = GLOBAL_RULES:get(name);
 		if (already_exists) then
 			print("overriding already existing rule: " .. name);
 		end
 
-		return GLOBAL_RULES:add(name, rule_fn, interval, state);
+		return GLOBAL_RULES:add(name, rule_fn, interval, state or {});
 	end
 
 	---@param rule Rule
 	---@return nil
 	function rules:begin(rule)
 		rule:begin();
+		return rule;
 	end
 
 	--- Takes a 'rule pattern', which is lua-style 'and' and 'or' with rule names, firing `callback` if the patten
@@ -151,7 +282,7 @@ if (H_CAMPAIGN == nil) then
 	--- Returns a LUA string to execute (we just leverage LUA's inbuilt language parsing).
 	---
 	---@param pattern string
-	---@param callback fun(rules: Rules): any
+	---@param callback Rule|RuleFn
 	---@return nil
 	function rules:on(pattern, callback)
 		-- 'A and (B or C)'
@@ -165,8 +296,16 @@ if (H_CAMPAIGN == nil) then
 			-- here we are constructing a LUA logic expression which will tell us the truthiness of the supplied pattern
 			-- if a rule is running, we insert true (1), else false (nil)
 			-- (we construct something like: "return 1 and nil and nil or (1 and 1)")
+			local p = %pattern;
 			pattern_exec = gsub(pattern_exec, "([%w_]+)", function(matches)
-				if(%rules:get(matches).status == "returned") then
+				local r = %rules:get(matches);
+				-- modkit.table.printTbl({
+				-- 	r = r or "nil",
+				-- 	pattern = %p,
+				-- 	pattern_exec = %pattern_exec,
+				-- 	matches = matches
+				-- }, matches);
+				if(r and (r.status == "resolved" or r.status == "rejected")) then
 					return "1";
 				end
 				return "nil";
@@ -182,6 +321,9 @@ if (H_CAMPAIGN == nil) then
 			print("overrriding already existing listener for [ " .. pattern .. " ]");
 		end
 
+		callback = toRuleFn(callback);
+
+		-- how to wait for this value?
 		GLOBAL_RULES.__listeners[pattern] = {
 			pattern = pattern,
 			exec = exec,
@@ -212,14 +354,18 @@ if (H_CAMPAIGN == nil) then
 	-- automatic rule, which checks for listener completion
 	function modkit_campaign_driver()
 		for _, listener in GLOBAL_RULES.__listeners do
+			-- print(listener.pattern .. ": " .. listener.exec());
 			if (dostring(listener.exec())) then
+				-- modkit.table.printTbl(listener);
 				print(listener.pattern .." passed conditions!");
-				listener.callback(%rules);
+				listener.callback();
 				GLOBAL_RULES.__listeners[listener.pattern] = nil; -- unsubscribe
 			end
 		end
 
-		
+		GLOBAL_RULES.__tick = GLOBAL_RULES.__tick + 1;
+
+
 		-- local new_universe_ships_group = Universe_GetAllActiveShips(SobGroup_Fresh("__mk_univese_ships"));
 		-- if (modkit.campaign.universe_ships_group) then
 		-- 	local diff_group = SobGroup_Fresh("__mk_universe_ships_diff");
